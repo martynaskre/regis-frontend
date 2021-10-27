4db23cc6bdcae9470fbaaf21f27d9418f53678bd
Nojus <53999653+Nojus15@users.noreply.github.com>
2021-10-27 20:58:20 +0300

    Test
    test

---
+++ pages/page.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>AAAAAA</h1>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator';
+import { AppModule } from '~/store';
+
+@Component
+export default class Index extends Vue {
+  mounted() {
+    AppModule.testAction();
+  }
+}
+</script>
