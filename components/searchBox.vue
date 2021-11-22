88822b5ca147d58805e0b381e643cfa5f32f468d
Nojus <53999653+Nojus15@users.noreply.github.com>
2021-11-22 18:56:46 +0200

    Main page seacrh box
    Main page seacrh box

--- components/searchBox.vue
+++ components/searchBox.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="background">
-    <input type="text" name="" id="" placeholder="Paslaugos" />
+    <div class="searchBar">
+      <input type="text" id="Paslaugos" placeholder="Paslaugos" />
+      <input type="text" id="Miestas" placeholder="Miestas" />
+      <button>
+        <div class="btn-square"></div>
+        <div class="btn-triangle"></div>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -8,9 +15,59 @@
 
 <style scoped>
 .background {
-  display: block;
+  display: flex;
   background-color: #6b9ac4;
   height: 550px;
   width: 100%;
+  justify-content: center;
+  align-items: center;
+}
+.searchBar {
+  height: 70px;
+  width: 50%;
+  border-radius: 15px;
+  display: inline-flex;
+  align-items: center;
+  background-color: white;
+  z-index: 11;
+}
+input {
+  z-index: 11;
+  margin: 0;
+  left: 0;
+}
+input:focus {
+  outline: none;
+}
+#Paslaugos {
+  width: 40%;
+  border-right: 1px solid #e1e1e1;
+}
+#Miestas {
+  width: 50%;
+}
+button {
+  width: 55px;
+  aspect-ratio: 1;
+  background: #4059ad;
+  border-radius: 50%;
+  margin-right: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-left: 15px;
+  margin: 0;
+}
+.btn-square {
+  width: 21px;
+  height: 7px;
+  background: #ffffff;
+}
+.btn-triangle {
+  width: 26px;
+  aspect-ratio: 1;
+  border-top: 13px solid transparent;
+  border-bottom: 13px solid transparent;
+  border-left: 13px solid #ffffff;
 }
 </style>
